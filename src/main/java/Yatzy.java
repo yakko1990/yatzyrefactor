c674c149fb7b9674816707acea06fa1b15c7fc7c
Robert Yassim da Cunha Viera <a23robertydcv@ateis.local>
2023-11-24 21:14:02 +0100

    Mensaje descriptivo de los cambios realizados

--- src/main/java/Yatzy.java
+++ src/main/java/Yatzy.java
@@ -3,11 +3,7 @@ public class Yatzy {
     public static int chance(int d1, int d2, int d3, int d4, int d5)
     {
         int total = 0;
-        total += d1;
-        total += d2;
-        total += d3;
-        total += d4;
-        total += d5;
+        total += d1 + d2 + d3 + d4 + d5;
         return total;
     }
 
