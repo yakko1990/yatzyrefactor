0ba3e402eae4018b4eee9263d62a8515b8b928ae
yakko1990 <ryassim18@gmail.com>
2023-12-15 10:02:23 +0100

    agrupe las variables en un vector y las sume en un acumulador

--- src/main/java/Yatzy.java
+++ src/main/java/Yatzy.java
@@ -2,8 +2,11 @@ public class Yatzy {
 
     public static int chance(int d1, int d2, int d3, int d4, int d5)
     {
+        int[] arraychance = {d1, d2, d3, d4, d5};
         int total = 0;
-        total += d1 + d2 + d3 + d4 + d5;
+        for (int i = 0; i < arraychance.length; i++) {
+            total += arraychance[i];
+        }
         return total;
     }
 
